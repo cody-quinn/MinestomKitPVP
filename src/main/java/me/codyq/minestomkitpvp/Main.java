d8356875021a91a1eb6fa6ceba8799ce2f38d01b
Luis <luisjk266@gmail.com>
2022-04-01 21:22:23 +0100

    feat: add project command

--- src/main/java/me/codyq/minestomkitpvp/Main.java
+++ src/main/java/me/codyq/minestomkitpvp/Main.java
@@ -3,6 +3,7 @@ package me.codyq.minestomkitpvp;
 import io.github.bloepiloepi.pvp.PvpExtension;
 import me.codyq.minestomkitpvp.commands.ClearCommand;
 import me.codyq.minestomkitpvp.commands.GameModeCommand;
+import me.codyq.minestomkitpvp.commands.ProjectCommand;
 import me.codyq.minestomkitpvp.commands.TeleportCommand;
 import me.codyq.minestomkitpvp.utils.KitUtils;
 import net.kyori.adventure.text.Component;
@@ -129,6 +130,7 @@ public class Main {
         commandManager.register(new TeleportCommand());
         commandManager.register(new GameModeCommand());
         commandManager.register(new ClearCommand());
+        commandManager.register(new ProjectCommand());
 
         ChatFilter.init();
 
