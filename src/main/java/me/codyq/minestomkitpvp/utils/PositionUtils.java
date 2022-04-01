886ed2aed4165f2c4c1c56b96c9f876021c139b4
CatDevz <codyquinn1122@gmail.com>
2022-04-01 01:58:38 -0500

    pog

--- src/main/java/me/codyq/minestomkitpvp/utils/PositionUtils.java
+++ src/main/java/me/codyq/minestomkitpvp/utils/PositionUtils.java
@@ -11,8 +11,8 @@ public class PositionUtils {
     public static Pos getRandomPos(InstanceContainer instanceContainer) {
         final Random random = new Random();
 
-        final int x = random.nextInt(-150, 150);
-        final int z = random.nextInt(-150, 150);
+        final int x = 260 + random.nextInt(-150, 150);
+        final int z = 60 + random.nextInt(-150, 150);
         final int y = 60;
 //        int y = 120;
 //        for (; y > 0; y--) {
